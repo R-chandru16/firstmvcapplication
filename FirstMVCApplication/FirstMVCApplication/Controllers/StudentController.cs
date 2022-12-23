0f20c56442899620ec1f5a9d296d94e14225a48c
Administrator <rchandru.rajendran@gmail.com>
2022-12-23 12:52:23 +0530

    first commit

---
+++ FirstMVCApplication/FirstMVCApplication/Controllers/StudentController.cs
@@ -0,0 +1,51 @@
+﻿using FirstMVCApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        static List<Student> students = new List<Student>();
+        public IActionResult Index()
+        {
+
+            //Action
+
+            //int[] score = { 100, 89, 97 };
+            //ViewBag.Name = "Tim";
+            //ViewData["Username"] = "userTim";
+            //ViewBag.Scores = score;
+            //ViewData["Scores"] = score;
+            //return View();
+
+            //Student student = new Student();
+            //student.Id = 101;
+            //student.Name = "tim ";
+            //student.Age = 21;
+            //ViewBag.student = student;
+            //return View(student);
+            return View(students);
+
+        }
+        [HttpGet] //Helpfull for getting input 
+        public IActionResult Create() // right click - view-RazorView-create.cshtml created
+        {
+            return View();
+        }
+  
+        [HttpPost] //Helpfull for post the output
+        public IActionResult Create(Student student)
+        {
+            students.Add(student);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(int id)
+        {
+            return View(id);
+        }
+    }
+}
